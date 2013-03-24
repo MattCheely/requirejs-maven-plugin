bcc90fa96bf79f2c232cfc2d12e36ab1530c6bd3
Sannie Kwakman <skwakman@xebia.com>
2013-03-24 15:05:24 +0100

    Moved single method from NodeJsProcess to NodeJsRunner, removing NodeJsProcess class

--- src/main/java/com/github/mcheely/maven/requirejs/NodeJsRunner.java
+++ src/main/java/com/github/mcheely/maven/requirejs/NodeJsRunner.java
@@ -1,5 +1,8 @@
 package com.github.mcheely.maven.requirejs;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
 import org.mozilla.javascript.ErrorReporter;
 
 import java.io.File;
@@ -14,11 +17,10 @@ public class NodeJsRunner implements Runner {
 
   @Override
   public ExitStatus exec(File mainScript, String[] args, ErrorReporter reporter) {
-    NodeJsProcess nodeJsProcess = new NodeJsProcess();
     ExitStatus exitStatus = new ExitStatus();
 
     try {
-      boolean result = nodeJsProcess.executeScript(nodeJsFile, mainScript.getAbsolutePath(), args);
+      boolean result = executeScript(nodeJsFile, mainScript.getAbsolutePath(), args);
       exitStatus.setExitCode(result?0:1);
 
     } catch (IOException e) {
@@ -26,5 +28,17 @@ public class NodeJsRunner implements Runner {
     }
 
     return exitStatus;
+  }
+
+  private boolean executeScript(String nodeJsFile, String scriptName, String[] params) throws IOException {
+    String line = nodeJsFile + " " + scriptName;
+    CommandLine cmdLine = CommandLine.parse(line);
+    cmdLine.addArguments(params);
+    DefaultExecutor executor = new DefaultExecutor();
+    try {
+      return executor.execute(cmdLine) == 0;
+    } catch (ExecuteException e) {
+      return e.getExitValue() == 0;
+    }
   }
 }
